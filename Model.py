8241c26b7bf2c5bb884096892b049b79ec74ff44
Matt-Woz <matt.wozakowski@gmail.com>
2021-12-01 20:50:15 +0000

    First commit - Created model file

---
+++ Model.py
@@ -0,0 +1,21 @@
+import string
+import re
+import codecs
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import feature_extraction
+from sklearn import linear_model
+from sklearn import pipeline
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+
+
+
+def main():
+
+
+    if __name__ == '__main__':
+        main()
